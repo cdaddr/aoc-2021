e15f8ed65f29e438565cb4ef596ad90d20e8307c
Brian Carper <brian@carper.ca>
2021-12-07 18:39:31 -0800

    add timing to util

--- rb/src/util.rb
+++ rb/src/util.rb
@@ -1,5 +1,8 @@
 def with_input
+  start_time = Time.now
   input_filename = 'input/' + File.basename($0).sub(/rb$/, 'txt')
   input = File.read(input_filename)
   yield input
+  end_time = Time.now
+  puts "elapsed time #{end_time - start_time}s"
 end
